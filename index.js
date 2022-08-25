409ef2cf4dc9d03b6b497cf434c17ac107b14fe3
leeyedam <dldpeka0909@gmail.com>
2022-08-25 23:43:30 +0900

    cors

--- index.js
+++ index.js
@@ -13,15 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 require("dotenv").config();
 
 app.use(cors());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+
 app.use(express.json());
 app.use(express.urlencoded());
 
