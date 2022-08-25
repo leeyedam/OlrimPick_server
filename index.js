fed96ce3aa059a48c03289e643c218b217e32de7
leeyedam <dldpeka0909@gmail.com>
2022-08-25 23:39:34 +0900

    cors

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const authRoutes = require("./routes/auth.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "views"));
+
 require("dotenv").config();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded());
-
-// const PORT = process.env.PORT || 5000;
-
-require("dotenv").config();
-
-const stream = require("getstream");
 
 app.get("/", (req, res) => {
   res.send("hi");
 });
+
+// require("dotenv").config();
+
+// const stream = require("getstream");
 
 app.use("/auth", authRoutes);
 
